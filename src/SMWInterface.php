8392cd000ad0b585dc2c3efe5fb0ce5b079eb4bb
Cindy Cicalese <cindom@gmail.com>
2021-06-04 19:38:45 -0400

    Refactor TitleIcon
    * new hook style
    * directory structure
    * separate SMW interface into separate class
    * dependency injection
    * service wiring
    * no direct access to globals
    * icon class in preparation for other icon types
    * unit test
    * phan
    
    Change-Id: If125eaea2a76585bee1f55768044b02f190b7724

---
+++ src/SMWInterface.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+namespace MediaWiki\Extension\TitleIcon;
+
+use ExtensionRegistry;
+use SMW\DIProperty;
+use SMW\DIWikiPage;
+use SMW\StoreFactory;
+use SMWDataItem;
+use Title;
+
+class SMWInterface {
+	/** @var bool */
+	private $isLoaded;
+
+	/**
+	 * @param ExtensionRegistry $extensionRegistry
+	 */
+	public function __construct(
+		ExtensionRegistry $extensionRegistry
+	) {
+		$this->isLoaded = $extensionRegistry->isLoaded( 'SemanticMediaWiki' );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isLoaded() : bool {
+		return $this->isLoaded;
+	}
+
+	/**
+	 * @param string $page
+	 * @param string $propertyName
+	 * @return string[]
+	 */
+	public function getPropertyValues( string $page, string $propertyName ) : array {
+		if ( !$this->isLoaded ) {
+			return [];
+		}
+
+		$store = StoreFactory::getStore();
+
+		$title = Title::newFromText( $page );
+
+		$subject = DIWikiPage::newFromTitle( $title );
+		$data = $store->getSemanticData( $subject );
+		$property = DIProperty::newFromUserLabel( $propertyName );
+		$values = $data->getPropertyValues( $property );
+
+		$strings = [];
+		foreach ( $values as $value ) {
+			if ( $value->getDIType() == SMWDataItem::TYPE_BLOB ) {
+				$strings[] = trim( $value->getString() );
+			}
+		}
+
+		return $strings;
+	}
+}
